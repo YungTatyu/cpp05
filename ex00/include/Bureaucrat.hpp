043414b93a0ebff12bde39bdebde2eabaec47a99
Tatsuki Terao <tterao@c1r16s7.42tokyo.jp>
2023-10-14 20:47:27 +0900

    init gogole test

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -1,6 +1,23 @@
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+#include <iostream>
+
 int	isEven(int n);
+
+class Bureaucrat
+{
+private:
+	const std::string	_name;
+	int	_grade;
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string& name);
+	Bureaucrat(const Bureaucrat& obj);
+	~Bureaucrat();
+	Bureaucrat&	operator=(const Bureaucrat &obj);
+	const std::string&	getName() const;
+	const int&	getGrade() const;
+};
 
 #endif
